8bf9d7834ada0903814bec4911c0a317d9770e26
Nawaf Alrumaihi <nawafalrumaihin@gmail.com>
2017-03-19 19:09:32 -0700

    Add files via upload

---
+++ beatsolve_drum.js
@@ -0,0 +1,81 @@
+function multipleChoice() {
+    var totalQuestions = 10;
+    var Questions =[10];
+    Questions[0]=["What is Tempo?"];
+    
+    Questions[1]=["What do we call the section of the song with the same lyrics and the same melody every-time?"];
+    
+    Questions[2]=["How many strings are on a guitar?"];
+    
+    Questions[3]=["What was the first song that was performed in outer space?"];
+    
+    Questions[4]=["How many notes are there (chromaticly) in an octave (including the first note repeated)?"];
+    
+    Questions[5]=["What do you use to calculate the speed of a song?"];
+    
+    Questions[6]=["What is harmony in music?"];
+    
+    Questions[7]=["What is the level and intensity of sound measured in?"];
+    
+    Questions[8]=["Which musical instrument has black and white keys?"];
+    
+    Questions[9]=["What is a ballad?"];
+    ///////////////////////////////////////////////////////////////////////
+    var choices=[10];
+    choices[0]=["The pace of the song", "The repeated sequences of a song", "The notes in a song","The volume of the song"];
+    
+    choices[1]=,["Bridge","Solo","Introduction","Chorus"];
+    
+    choices[2]=[6,4,8,12];
+    
+    choices[3]=["Happy Birthday","Hotel California","Rolling in the Deep","Is This Love?"];
+    
+    choices[4]=[10,9,8,7];
+    
+    choices[5]=["Metronome","Guessing","Playing an instrument","Asking a instructor"];
+    
+    choices[6]=["The strong, regular, repeated pattern of movement or sound", "The combination of simultaneously sounded musical notes to produce chords and chord progressions having a pleasing effect","A sequence of single notes that is musically satisfying","The character or quality of a musical sound or voice as distinct from its pitch and intensity"];
+    
+    choices[7]=["Sound level meter","Decibels","Frequency","Distance"];
+    
+    choices[8]=["Ukuele","Drumset","Piano","Tabla"];
+    
+    choices[9]=["A short poem or other set of words set to music or meant to be sung.","A poem or song narrating a story in short stanzas.","(of music) performed on instruments, not sung.","a group of lines forming the basic recurring metrical unit in a poem; a verse."];
+    /////////////////////////////////////////////////////////////////////
+    var answers=[10];
+    answers[0]=["The pace of the song"];
+    
+    answers[1]=["Chorus"];
+    
+    answers[2]=[6];
+    
+    answers[3]=["Happy Birthday"];
+    
+    answers[4]=[7];
+    
+    answers[5]=["Metronome"];
+    
+    answers[6]=["The strong, regular, repeated pattern of movement or sound", "The combination of simultaneously sounded musical notes to produce chords and chord progressions having a pleasing effect"];
+    
+    answers[7]=["Decibels"];
+    
+    answers[8]=["Piano"];
+    
+    answers[9]=["A poem or song narrating a story in short stanzas."];
+    /////////////////////////////////////////////////////////////////////
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
