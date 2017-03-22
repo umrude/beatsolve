0c44e976d817070db67796b9697e209157dc6c5d
umrude <gabigeollegue@gmail.com>
2017-03-22 10:26:27 -0700

    Add files via upload

---
+++ beatsolve2bass.js
@@ -0,0 +1,43 @@
+var questions = [{
+   
+    question : "Why does JavaScript and Java have similar names?",
+    choices : ["Javascript is a stripped-down version of Java", "JavaScript's syntax is loosely based on Java's", "They both orginated ont he island of Java", "The creators loved coffee"],
+    correctAnswer: 1
+}, {
+    question: "What are variables used for in JavaScript programs?",
+    choices: ["Storing numbers, dates or other values", "Varying randomnly", "Causing high-school alegra flashbacks", "Creating variation in a function"],
+    correctAnswer: 0
+}, {
+    question: "What is the correct JavaScript syntax to write 'Hello World'?",
+    choices: ["System.out.println('Hello World)", "write.me'Hello World'", "document.write('Hello World')", "response.write('Hello World')"],
+    correctAnswer: 2
+}, {
+    question: "Inside which HTML element do we put the JavaScript",
+    choices: ["<script>", "<js>", "<javascript>", "<dothing>"],
+    correctAnswer: 0
+}, {
+    question: "The _______ method of an Array object adds and/or removes elements from an array.",
+    choices: ["remove", "deleteMe", "splice", "cut"],
+    correctAnswer: 2
+}, {
+    question: "Using _______ statement is how you test for a specific condition.",
+    choices: ["where", "if", "then", "do"],
+    correctAnswer: 1
+}, {
+    question: "How does JavaScript store dates in a date object?",
+    choices: ["The number of milliseconds since January 1st, 1970", "The number of seconds since the earth's first rotation", "It looks at a clock", "Number of days since January 1st, 1899"],
+    correctAnswer: 0
+}, {
+    question: "Is it possible to nest functions in JavaScript?",
+    choices: ["Yes", "No", "Only wher birds are invloved in the function", "nesteption"],
+    correctAnswer: 0
+}, {
+    question: "What is the correct syntax for referring to an external script called 'abc.js'?",
+    choices: ["<script href='abc.js'>", "<script name='abc.js'>", "<script src='abc.js'>", "None of the above"],
+    correctAnswer: 2
+}, {
+    question: "Which of the following is not a valid JavaScript variable name?",
+    choices: ["2names", "_first_and_last_names", "FirstAndLast", "None of the above"],
+    correctAnswer: 0
+}];
+
